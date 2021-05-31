30e375c28bd4520c161edabfb287be52276d09ec
Aditya Amartya <aditya.amartya@naukri.com>
2021-05-31 20:37:07 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMM_App"
+include(":androidApp")
+include(":shared")
